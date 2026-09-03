0c62f4d80144e8f1d55d9e194ba28906dc916121
osvat0rres <osvaldotorres3108@gmail.com>
2026-09-02 22:09:44 -0700

    update main application

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,15 +5,25 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 import MovieCard from './componets/MovieCard'
 import Home from './pages/home'
+import { Routes, Route } from 'react-router-dom'
+import Favorites from './pages/favorites'; 
+import NavBar from './componets/NavBar';
+
+
 
 function App() {
 
   return (
-    //fragment 
-    <>
-      <Home/>
-    </>
-  ) 
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/favorites' element={<Favorites/>}/>
+        </Routes>
+      </main>
+    </div>
+  ); 
 }
 
  
---
+++ front-end/src/pages/favorites.jsx
@@ -0,0 +1,7 @@
+function Favorites(){
+    return <div className="favorites-empty">
+        <h2>No faorities Movies yet</h2>
+        <p>Start adding movies here</p>
+    </div>
+} 
+export default Favorites
